0aa2b328bd15ef6cc404f60477ea4687a72f5a08
Ben Shaver <benpshaver@gmail.com>
2023-04-19 13:39:13 -0400

    feat: Started implementation of TaskTree trait on Vec<Task>, also moved io to task.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod cli;
-mod io;
 mod task;
 
 use crate::cli::{Args, TntCommand};
+use crate::task::TaskTree;
+use std::path::PathBuf;
 
 fn main() {
+    let path = PathBuf::from("/Users/bshaver/.tnt.json");
+    let tasks = task::read_task_list_from_file(path).expect("~/.tnt.json exists");
     let args = Args::parse_args();
     match args.command {
         None => println!("No subcommand provided, showing current task..."),
@@ -19,6 +22,10 @@ fn main() {
                     name, parent, switch
                 )
             }
+            TntCommand::View => match tasks.get_active_task() {
+                None => println!("No active task"),
+                Some(task) => println!("{}", task),
+            },
             _ => todo!("Command not supported yet!"),
         },
     }
--- src/task.rs
+++ src/task.rs
@@ -1,8 +1,9 @@
-use aoc_utils::tree::ArenaTree;
-
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
-use std::default::Default;
+use std::fmt;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Task {
@@ -14,22 +15,111 @@ pub struct Task {
     done: bool,
 }
 
+pub trait TaskTree {
+    fn get_root_tasks(&self) -> Vec<&Task>;
+    fn get_leaf_tasks(&self) -> Vec<&Task>;
+    fn get_active_task(&self) -> Option<&Task>;
+    fn set_active_task(&mut self, id: usize);
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+impl TaskTree for Vec<Task> {
+    fn get_root_tasks(&self) -> Vec<&Task> {
+        self.iter()
+            .filter(|t| t.parent.is_none() && !t.done)
+            .collect()
+    }
+    fn get_leaf_tasks(&self) -> Vec<&Task> {
+        self.iter()
+            .filter(|t| t.children.is_empty() && !t.done)
+            .collect()
+    }
+    fn get_active_task(&self) -> Option<&Task> {
+        self.iter().find(|t| t.active)
+    }
+    fn set_active_task(&mut self, id: usize) {
+        self.iter_mut().for_each(|task| task.active = task.id == id);
+    }
+}
+
+pub fn read_task_list_from_file(file: PathBuf) -> Result<Vec<Task>> {
+    let f = File::open(file)?;
+    let reader = BufReader::new(f);
+    let task_list: Vec<Task> = serde_json::from_reader(reader)?;
+    Ok(task_list)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_init_empty_arena_tree_and_add_one_node() {
-        let mut tree: ArenaTree<isize> = ArenaTree::new();
-        assert_eq!(tree.len(), 0);
-        tree.add_node(42);
-        assert_eq!(tree.len(), 1);
+    fn tasks_fixture() -> Vec<Task> {
+        vec![
+            Task {
+                id: 0,
+                value: "foo".to_string(),
+                parent: None,
+                children: vec![2],
+                active: false,
+                done: false,
+            },
+            Task {
+                id: 1,
+                value: "bar".to_string(),
+                parent: None,
+                children: vec![],
+                active: false,
+                done: true,
+            },
+            Task {
+                id: 2,
+                value: "baz".to_string(),
+                parent: Some(0),
+                children: vec![],
+                active: true,
+                done: false,
+            },
+        ]
+    }
+    fn task_list_fixture() -> PathBuf {
+        let mut file = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        file.push("tests/test_data/test_tasks.json");
+        file
     }
 
     #[test]
-    fn test_can_init_default_empty_task_list() {
-        let tasks: Vec<Task> = Vec::default();
-        assert_eq!(tasks.len(), 0)
+    fn test_get_root_tasks() {
+        assert_eq!(tasks_fixture().get_root_tasks()[0].id, 0);
+        assert_eq!(tasks_fixture().get_root_tasks().len(), 1);
+    }
+
+    #[test]
+    fn test_get_leaf_tasks() {
+        assert_eq!(tasks_fixture().get_leaf_tasks()[0].id, 2);
+        assert_eq!(tasks_fixture().get_leaf_tasks().len(), 1);
+    }
+    #[test]
+    fn test_get_active_task() {
+        assert_eq!(tasks_fixture().get_active_task().unwrap().id, 2);
+    }
+
+    #[test]
+    fn test_set_active_task() {
+        let mut tasks = vec![Task {
+            id: 0,
+            value: "foo".to_string(),
+            parent: None,
+            children: vec![],
+            done: false,
+            active: false,
+        }];
+        tasks.set_active_task(0);
+        assert!(tasks[0].active)
     }
 
     #[test]
@@ -48,7 +138,15 @@ mod tests {
         assert_eq!(task.value, "do my taxes");
         assert_eq!(task.parent, None);
         assert_eq!(task.children, vec![6]);
-        assert_eq!(task.active, false);
-        assert_eq!(task.done, false);
+        assert!(!task.active);
+        assert!(!task.done);
+    }
+
+    #[test]
+    fn test_read_lines() {
+        let tasks: Vec<Task> = read_task_list_from_file(task_list_fixture()).unwrap();
+        assert_eq!(tasks.len(), 2);
+        assert_eq!(tasks[0].value, "do my taxes");
+        assert_eq!(tasks[1].value, "get w2");
     }
 }
